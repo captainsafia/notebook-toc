f6336b60bf0b7e5969c184f593709ee3b2baf41c
Safia Abdalla <safia@safia.rocks>
2016-03-23 17:11:34 -0500

    Clean up heading cells before updating

--- notebook-toc.js
+++ notebook-toc.js
@@ -8,18 +8,25 @@ define(['base/js/namespace', 'base/js/utils', 'jquery'], function(Jupyter, utils
             if (cell.cell_type == "markdown") {
                var cell_content = cell.get_text(); 
                if (cell_content.startsWith("#")) {
+                   // Increment the heading count
                    heading_count++;
-
+                   // Get the heading title
                    var last_hash = cell_content.lastIndexOf("#");
                    var hash_num = last_hash + 1;
                    var heading_title = cell_content.substr(hash_num);
-
+                   // Clean up the existing heading content
+                   var first_tag = heading_title.indexOf("<");
+                   console.log(first_tag);
+                   heading_title = heading_title.slice(0, first_tag);
+                   console.log(heading_title);
+                   // Create a <a> tag for linking to fragment
                    var link_fragment_heading = heading_title.toLowerCase().replace(" ", "_");
                    var link_fragment = '<a name="' + link_fragment_heading + '"></a>';
-
+                   console.log(link_fragment);
+                   // Append the <a> tag to the heading cell
                    cell.set_text(cell.get_text() +  link_fragment);
                    cell.render();
-
+                   // Add the heading to the TOC cell
                    toc_text += (heading_count + ". ");
                    toc_text += ("[" + heading_title + "](#" + link_fragment_heading + ") \n");
                }
